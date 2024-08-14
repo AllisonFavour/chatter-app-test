3a9042437aeb378ba976f5e1defa14b5417feca7
Allison Favour <allisonfavour@outlook.com>
2024-08-13 23:55:45 -0700

    still trying to deploy on vercel

--- app/(components)/PostCard.tsx
+++ app/(components)/PostCard.tsx
@@ -1,28 +1,28 @@
-// "use client";
+"use client";
 
-// import { useState } from "react";
+import { useState } from "react";
 
-// export default function PostCard({post}: {post: any}) {
-//     const [likes, setLikes] = useState(post.likes);
+export default function PostCard({post}: {post: any}) {
+    const [likes, setLikes] = useState(post.likes);
 
-//     const handleLikes = async () => {
-//         await fetch('/api/likes/', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({postId: post._id}),
-//         });
+    const handleLikes = async () => {
+        await fetch('/api/likes/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({postId: post._id}),
+        });
 
-//         setLikes(likes+1);
-//     };
+        setLikes(likes+1);
+    };
 
-//     return (
-//         <div>
-//             <h2>{post.title}</h2>
-//             <p>{post.content}</p>
-//             <button onClick={handleLikes}>Like ({likes})</button>
-//             {/* bookmark functionality */}
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <button onClick={handleLikes}>Like ({likes})</button>
+            {/* bookmark functionality */}
+        </div>
+    )
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,39 +1,50 @@
-// "use client";
+"use client";
 
-// import { useEffect, useState } from "react";
-// // import PostCard from "../(components)/PostCard";
+import { useEffect, useState } from "react";
+import PostCard from "../(components)/PostCard";
 
-// export default function PostsPage() {
-//   const [posts, setPosts] = useState([]);
-//   const [loading, setLoading] = useState(true);
 
-//   useEffect(() => {
-//     const fetchPosts = async () => {
-//       const res = await fetch("api/posts/");
-//       const data = await res.json();
-//       setPosts(data);
-//       setLoading(false);
-//     };
+interface Post {
+    _id: string;
+    title: string;
+    content: string;
+    author: string;
+    createdAt: string;
+    updatedAt: string;
+  }
 
-//     fetchPosts();
-//   }, []);
 
-//   if (loading) return <p>Loading...</p>;
+export default function PostsPage() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
 
-//   return (
-//     <>
-//       <div>
-//         <h1>All blog posts</h1>
-//         {posts.length === 0 ? (
-//           <p>No post available yet, make your first post.</p>
-//         ) : (
-//           <div>
-//             {posts.map((post) => (
-//               <PostCard key={post._id} post={post} />
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("api/posts/");
+      const data = await res.json();
+      setPosts(data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <div>
+        <h1>All blog posts</h1>
+        {posts.length === 0 ? (
+          <p>No post available yet, make your first post.</p>
+        ) : (
+          <div>
+            {posts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}

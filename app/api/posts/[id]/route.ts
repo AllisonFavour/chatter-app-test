8ccb9d3e706bcfc783f9a8e1a44b1c1441d38d73
Allison Favour <allisonfavour@outlook.com>
2024-08-13 21:58:03 -0700

    trying to deploy to vercel

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,15 +1,15 @@
-// to fetch a single post
-import { NextRequest, NextResponse } from "next/server";
-import connectToDatabase from "@/lib/mongoose";
-import Post from "@/models/Post";
+// // to fetch a single post
+// import { NextRequest, NextResponse } from "next/server";
+// import connectToDatabase from "@/lib/mongoose";
+// import Post from "@/models/Post";
 
 
-export async function GET(req: NextRequest) {
-    const {postId} = req.nextUrl.pathname.split('/').pop() as string;
+// export async function GET(req: NextRequest) {
+//     const {postId} = req.nextUrl.pathname.split('/').pop() as string;
 
-    await connectToDatabase();
+//     await connectToDatabase();
 
-    const post = Post.findById(postId).populate('comments').exec();
+//     const post = Post.findById(postId).populate('comments').exec();
 
-    return NextResponse.json(post);
-}
+//     return NextResponse.json(post);
+// }
